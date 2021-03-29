7da8410358bed3ec71e409843eba5f56290f8a3a
Sayed Sadat <sayedsadat344@gmail.com>
2021-03-29 11:31:14 +0430

    laravel debugger added

--- resources/views/layouts/front-layout.blade.php
+++ resources/views/layouts/front-layout.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @livewireStyles
 </head>
 <body>
     <div id="app">
@@ -45,5 +46,7 @@
             @yield('content')
         </main>
     </div>
+
+    @livewireScripts
 </body>
 </html>
